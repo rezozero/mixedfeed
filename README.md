5d2d5fbd79a57eef8f0bbbf9e1c73e869d9e2990
Ambroise Maupate <ambroise@rezo-zero.com>
2015-06-19 12:07:56 +0200

    Added base class structure

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # mixedfeed
 A PHP library to get social networks feeds and merge them 
+
+![License](http://img.shields.io/:license-mit-blue.svg)
+
+## Install
+
+```shell
+composer require rezozero/mixedfeed
+```
+
+```php
+use RZ\MixedFeed\MixedFeed;
+use RZ\MixedFeed\InstagramFeed;
+use RZ\MixedFeed\TwitterFeed;
+
+$feed = new MixedFeed([
+    new InstagramFeed(
+        'instagram_user_id',
+        'instagram_access_token'
+        // you can add a doctrine cache provider
+    ),
+    new TwitterFeed(
+        'twitter_user_id',
+        'twitter_consumer_key',
+        'twitter_consumer_secret',
+        'twitter_access_token',
+        'twitter_access_token_secret'
+        // you can add a doctrine cache provider
+    ),
+]);
+
+return $feed->getItems(12);
+```
+
+## Combine feeds
+
+*mixedfeed* can combine multiple social feeds so you can loop over them and use some common data fields such as `feedItemPlatform` and `normalizedDate`.
+
+Each feed provider much add these two parameters in feed items:
+
+* `feedItemPlatform`: This is your social network name as a *string* i.e. «twitter». It will be important to cache your feed and for your HTML template engine to render properly each feed item.
+
+For example, if you are using *Twig*, you will be able to include a sub-template for each social-platform.
+
+```twig
+{% for socialItem in mixedFeedItems %}
+{% include 'social-blocks/' ~ socialItem.feedItemPlatform ~ '.html.twig' %}
+{% endfor %}
+```
+
+* `normalizedDate`: This is a crucial parameter as it allows *mixedfeed* library to sort *antechronologically* multiple feeds with heterogeneous structures.
---
+++ src/InstagramFeed.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright © 2015, Ambroise Maupate
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @file InstagramFeed.php
+ * @author Ambroise Maupate
+ */
+namespace RZ\MixedFeed;
+
+use Doctrine\Common\Cache\CacheProvider;
+use RZ\MixedFeed\AbstractFeedProvider;
+
+/**
+ * Get an Instagram user feed.
+ */
+class InstagramFeed extends AbstractFeedProvider
+{
+    protected $userId;
+    protected $accessToken;
+    protected $cacheProvider;
+    protected $cacheKey;
+    protected static $timeKey = 'created_time';
+
+    public function __construct($userId, $accessToken, CacheProvider $cacheProvider = null)
+    {
+        $this->userId = $userId;
+        $this->accessToken = $accessToken;
+        $this->cacheProvider = $cacheProvider;
+        $this->cacheKey = $this->getFeedPlatform() . $this->userId;
+    }
+
+    protected function getFeed($count = 5)
+    {
+        if (null !== $this->cacheProvider &&
+            $this->cacheProvider->contains($this->cacheKey . $count)) {
+            return $this->cacheProvider->fetch($this->cacheKey . $count);
+        }
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get('https://api.instagram.com/v1/users/' . $this->userId . '/media/recent/', [
+            'query' => [
+                'access_token' => $this->accessToken,
+                'count' => $count,
+            ],
+        ]);
+
+        $body = json_decode($response->getBody());
+
+        if (null !== $this->cacheProvider) {
+            $this->cacheProvider->save(
+                $this->cacheKey . $count,
+                $body,
+                7200
+            );
+        }
+
+        return $body->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateTime($item)
+    {
+        $date = new \DateTime();
+        $date->setTimestamp($item->created_time);
+        return $date;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFeedPlatform()
+    {
+        return 'instagram';
+    }
+}
